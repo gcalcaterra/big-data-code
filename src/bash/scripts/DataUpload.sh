01361761424ee97768b18ceef770d9c54f7fc5c3
Gustavo Calcaterra <gcalcaterra@data.com.py>
2018-09-23 10:59:13 -0400

    DataUpload.sh working

--- src/bash/scripts/DataUpload.sh
+++ src/bash/scripts/DataUpload.sh
@@ -1,10 +1,11 @@
 #Borra los datos y la salida del hdfs para que no cree conflicto si ya existe
-hdfs dfs -rm /datos
+hdfs dfs -rm /internet-use-data-*
 
 #Copia el archivo con los datos al hdfs
-hdfs dfs -copyFromLocal ../../../../../datos/internet-use-data-2016.csv /datos/internet-use-data-2016.csv
+hdfs dfs -copyFromLocal ../../../data/internet-use-data-2016.csv /internet-use-data-2016
 
 #Muestra la salida del programa
-echo Salida de la carga de datos en bruto
-hdfs dfs -cat /datos/*
-
+#echo Salida de la carga de datos en bruto
+#hdfs dfs -cat /internet-use-data*
+echo Verificacion del archivo cargado en el hdfs:
+hdfs dfs -ls /internet-use-data-2016
