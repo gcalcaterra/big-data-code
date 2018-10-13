09dc9003f13685f14b9f187865fd73d87d2edb49
Angel Rafael Acosta Cardozo <angel.rac777@gmail.com>
2018-10-13 19:49:33 -0300

    Genera archivos csv

--- batchlayer/src/java/tpBigData/Main.java
+++ batchlayer/src/java/tpBigData/Main.java
@@ -23,77 +23,101 @@ import static tpBigData.test.DataProcess.makeFacts;
 public class Main {
     public static void main(String args[]) throws Exception {
         if (args.length != 1  ) {
-            System.out.println("Modo de uso:");
+            System.out.println("Modos de uso:");
             System.out.println("hadoop jar <path/al/.jar> <path/a/data>");
+            System.out.println("hadoop jar --show-data");
             return;
         }
 
-        if (!new File(args[0]).exists()){
-            System.out.println("No existe el directorio: " + args[0]);
-            return;
+        if (args[0].equals("--show-data")) {
+            showData();
+        } else {
+
+            if (!new File(args[0]).exists()) {
+                System.out.println("No existe el directorio: " + args[0]);
+                return;
+            }
+
+            String path = args[0];
+
+            while (true) {
+                System.out.println("Insertando los datos nuevos al serving layer");
+
+                insertDataFromCSVtoHDFS(path);
+                batchWorkflow();
+                batchViews();
+
+                System.out.println("Se insertaron los datos nuevos al serving layer. Esperando 30 segs...");
+                Thread.sleep(30000);
+
+            }
         }
-
-        String path = args[0];
-
-        initTestData(path);
-        batchWorkflow();
-        batchViews();
-        //showData();
     }
 
-    public static void initTestData(String path) throws Exception {
+    public static void insertDataFromCSVtoHDFS(String path) throws Exception {
         FileSystem fs = FileSystem.get(new Configuration());
         fs.delete(new Path(BatchWorkflow.DATA_ROOT), true);
         fs.mkdirs(new Path(BatchWorkflow.DATA_ROOT));
 
-        Pail masterPail = Pail.create(BatchWorkflow.MASTER_ROOT, new SplitDataPailStructure());
+        Pail masterPail;
+        try {
+            //Si ya existe un pail en MASTER_ROOT
+            masterPail = new Pail(BatchWorkflow.MASTER_ROOT);
+        } catch (Exception e) {
+            //Si todavía no existe un pail en MASTER_ROOT
+            masterPail = Pail.create(BatchWorkflow.MASTER_ROOT, new SplitDataPailStructure());
+        }
+
         Pail newPail = Pail.create(BatchWorkflow.NEW_ROOT, new DataPailStructure());
 
         Pail.TypedRecordOutputStream os;
 
         //Carga al hdfs los archivos de la carpeta indicada en path
         File f = new File(path);
-        File[] files = f.listFiles();
+        File[] files = f.listFiles((File pathname) -> pathname.getName().endsWith(".csv"));
 
-        for (File file : files) {
-            BufferedReader reader = null;
+        if (files != null) {
+            for (File file : files) {
+                BufferedReader reader = null;
 
-            try {
-                os = newPail.openWrite();
+                try {
+                    os = newPail.openWrite();
 
-                reader = new BufferedReader(new FileReader(file));
-                String line = reader.readLine(); // Lee la cabecera y la ignora
+                    reader = new BufferedReader(new FileReader(file));
+                    String line = reader.readLine(); // Lee la cabecera y la ignora
 
-                while ((line = reader.readLine()) != null) {
-                    String[] tokens = line.split(",");
+                    while ((line = reader.readLine()) != null) {
+                        String[] tokens = line.split(",");
 
-                    long timeSecs = new Date().getTime();
-                    String internetUseId = tokens[0];
-                    String individualTypeId = tokens[1];
-                    String geographyId = tokens[2];
-                    int year = Integer.parseInt(tokens[3]);
-                    int units = Integer.parseInt(
-                                    tokens[4]
-                                    .replace("u", "")
-                                    .replace("b", "")
-                                    .replace("n", "")
-                                    .replace(":", "-1")
-                                    .trim()
-                    );
+                        long timeSecs = new Date().getTime();
+                        String internetUseId = tokens[0];
+                        String individualTypeId = tokens[1];
+                        String geographyId = tokens[2];
+                        int year = Integer.parseInt(tokens[3]);
+                        int units = Integer.parseInt(
+                                tokens[4]
+                                        .replace("u", "")
+                                        .replace("b", "")
+                                        .replace("n", "")
+                                        .replace(":", "-1")
+                                        .trim()
+                        );
 
 
-                    os.writeObject(makeFacts(timeSecs, internetUseId, individualTypeId, geographyId, year, units));
+                        os.writeObject(makeFacts(timeSecs, internetUseId, individualTypeId, geographyId, year, units));
 
+                    }
+
+                    os.close();
+
+                    file.delete();
+
+                } finally {
+                    if (reader != null) {
+                        reader.close();
+                    }
 
                 }
-
-                os.close();
-
-            } finally {
-                if (reader != null) {
-                    reader.close();
-                }
-
             }
         }
     }
--- speedlayer/src/main/java/tpBigdata/rest/InternetUseRest.java
+++ speedlayer/src/main/java/tpBigdata/rest/InternetUseRest.java
@@ -1,50 +1,38 @@
 package tpBigdata.rest;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import tpBigdata.ejb.InternetUseDAO;
+import tpBigdata.model.InternetUse;
+import tpBigdata.model.InternetUseView;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import tpBigdata.ejb.InternetUseDAO;
-import tpBigdata.model.InternetUse;
-import tpBigdata.model.InternetUseView;
-/*
-import com.backtype.hadoop.pail.Pail;
-import tpBigData.batchlayer.BatchWorkflow;
-import tpBigData.tap.DataPailStructure;
-import tpBigData.tap.SplitDataPailStructure;
+import static java.util.UUID.randomUUID;
 
-import static tpBigData.test.DataProcess.makeFacts;
-
-*/
 
 @Path("internetUse")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class InternetUseRest {
+	public static final String CSV_DATA = "/tmp/data/";
+
 	@EJB
 	private InternetUseDAO internetUseDAO;
 	@Context
@@ -60,44 +48,50 @@ public class InternetUseRest {
 		Map<String,Object> mapaResultado= new HashMap<>();
 		mapaResultado.put("total", total);
 		mapaResultado.put("lista", listEntity);
-		
+
 		return Response.ok(mapaResultado).build();
-		
 	}
 
 	@POST
 	@Path("/")
 	public Response agregar(InternetUse internetUse) throws WebApplicationException {
-
-		/*
-		//Insertar en el HDFS
+		//Guarda el resultado en un archivo .csv
+		File archivoCSV = new File(CSV_DATA + randomUUID() + ".csv");
+		CSVPrinter csvPrinter;
 		try {
-			Pail newPail = new Pail(BatchWorkflow.NEW_ROOT);
-			Pail.TypedRecordOutputStream os = newPail.openWrite();
+			new File(CSV_DATA).mkdirs();
+			archivoCSV.createNewFile();
 
-			String internetUseId = internetUse.getInternetUseId();
-			String individualTypeId = internetUse.getIndividualTypeId();
-			String geographyId = internetUse.getGeographyId();
-			int year = internetUse.getYear();
-			int units = internetUse.getUnits();
-			long timeSecs = new  Date().getTime();
+			csvPrinter = CSVFormat.EXCEL
+					.withHeader("indic_is", "ind_type", "geo\\time", "year", "percent")
+					.print(archivoCSV, Charset.forName("UTF-8"));
 
-			os.writeObject(makeFacts(timeSecs, internetUseId, individualTypeId, geographyId, year, units));
-			os.close();
+			ArrayList<String> record = new ArrayList<>();
+
+			record.add(internetUse.getInternetUseId());
+			record.add(internetUse.getIndividualTypeId());
+			record.add(internetUse.getGeographyId());
+			record.add(Integer.toString(internetUse.getYear()));
+			record.add(Integer.toString(internetUse.getUnits()));
+
+			csvPrinter.printRecord(record);
+			csvPrinter.close();
+
+		} catch (IOException e) {
+			throw new WebApplicationException(e);
+		}
+
+		//Responde con estado 201 (Creado)
+		try {
+			UriBuilder resourcePathBuilder = UriBuilder.fromUri(uriInfo
+					.getAbsolutePath());
+
+			URI resourceUri = resourcePathBuilder
+					.path(URLEncoder.encode(internetUse.getInternetUseId(), "UTF-8")).build();
+			return Response.created(resourceUri).build();
 		} catch (Exception e) {
 			throw new WebApplicationException(e);
 		}
-			*/
-		UriBuilder resourcePathBuilder = UriBuilder.fromUri(uriInfo
-				.getAbsolutePath());
-		URI resourceUri=null;
-		try {
-			resourceUri = resourcePathBuilder
-					.path(URLEncoder.encode(internetUse.getInternetUseId(), "UTF-8")).build();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return Response.created(resourceUri).build();
 	}
 
 }
